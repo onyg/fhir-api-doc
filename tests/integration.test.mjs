40b13d8059fbe0dd07d57d6633f43ebabf328d5b
FredrikSchaefer <fredrik.schaefer@gematik.de>
2025-12-04 07:40:06 +0000

    Integration test disclaimer

--- tests/integration.test.mjs
+++ tests/integration.test.mjs
@@ -3,6 +3,12 @@ import fs from 'fs';
 import path from 'path';
 import capture from './helpers/capture.mjs'
 
+/* 
+    This test renders a predefined set of content stored in ./dev-helpers and checks if the result equals a previously generated 'golden' one.
+    If you make changes to the code, this test is likely to fail.
+    You can update the golden version of generated files with 'npm run updateGoldenSnapshot'.
+    Please verify that all changes to the resulting ./tests/golden-snapshot are intended!
+*/
 describe('All golden test websites', () => {
 
     it('should be unchanged when rendered with current logic', async () => {
